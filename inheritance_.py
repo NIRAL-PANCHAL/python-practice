544c20b467a340f126740c171a3cdec332af755f
NIRAL-PANCHAL <106137962+NIRAL-PANCHAL@users.noreply.github.com>
2022-05-24 12:07:17 +0530

    Update inheritance_.py

--- inheritance_.py
+++ inheritance_.py
@@ -1,12 +1,12 @@
-class person:
-    def __init__(self, fname, lname):
+class person: #base class
+    def __init__(self, fname, lname): #create constructor
         self.fname = fname
         self.lname = lname
     
-    def printname(self):
+    def printname(self): #create function
         print(self.fname,self.lname)
-class student(person):
+class student(person): #inherit base class
     def __init__(self, fname, lname):
-        person.__init__(self, fname, lname)
+        person.__init__(self, fname, lname) 
 x = student("alex ", "pandey")
 x.printname()
