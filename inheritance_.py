6551110978edc55161b4b8f7e386b489a1c63994
Niral <niralpanchal0412@gmail.com>
2022-05-25 10:51:58 +0530

    add the inheritance

--- inheritance_.py
+++ inheritance_.py
@@ -1,12 +1,12 @@
 class person:
-    def __init__(self, fname, lname):
+    def __init__(self, fname, lname): #constructor created
         self.fname = fname
         self.lname = lname
     
-    def printname(self):
+    def printname(self): #function created
         print(self.fname,self.lname)
-class student(person):
-    def __init__(self, fname, lname):
+class student(person): #child class / inherit class
+    def __init__(self, fname, lname): 
         person.__init__(self, fname, lname)
-x = student("alex ", "pandey")
-x.printname()
+x = student("alex ", "pandey") #assign the value
+x.printname() #calling function
